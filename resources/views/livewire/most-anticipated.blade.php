7a77979b5a4666825282f5a798ea8fd4b4166c95
madhiyono <yonclr97@gmail.com>
2021-12-15 14:28:53 +0700

    Details Game

--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -2,7 +2,7 @@
     @forelse ($mostAnticipated as $game)
         <div class="game flex">
             <div class="flex-none">
-                <a href="#">
+                <a href="{{ route('games.show', $game['slug']) }}">
                     @if (array_key_exists('cover', $game))
                         <img src="{{ Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']) }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
                     @else
@@ -11,7 +11,7 @@
                 </a>
             </div>
             <div class="ml-4">
-                <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
+                <a href="{{ route('games.show', $game['slug']) }}" class="hover:text-gray-300">{{ $game['name'] }}</a>
                 <div class="text-gray-400 text-sm mt-1">{{ Carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}</div>
             </div>
         </div>
